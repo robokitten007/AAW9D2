e7d041f9054ee85df32bdb25512ce094bf517934
wliu <wliu0709@gmail.com>
2021-06-15 15:33:10 -0400

    finished tictac

--- skeleton/src/index.js
+++ skeleton/src/index.js
@@ -1,6 +1,10 @@
-const View = // require appropriate file
-const Game = // require appropriate file
+
+const View = require('./ttt-view.js') // require appropriate file
+const Game = require('../ttt_node/game.js')// require appropriate file
 
 document.addEventListener("DOMContentLoaded", () => {
+  const game = new Game();
+  let el = document.querySelector('.ttt');
+  const view = new View(game, el);
   // Your code here
 });
--- skeleton/src/ttt-view.js
+++ skeleton/src/ttt-view.js
@@ -1,13 +1,71 @@
+const Board = require("../ttt_node/board");
+const Game = require("../ttt_node/game");
 class View {
-  constructor(game, el) {}
+  constructor(game, el) {
+    this.game = game;
+    this.el = el;
+    el.appendChild(this.setupBoard());
+    this.bindEvents();
+  }
 
-  setupBoard() {}
+  setupBoard() {
+    let ul = document.createElement('UL');
+    ul.className = "ttt-board";
+    let children = [0,1,2,3,4,5,6,7,8];
+    children.forEach(child => {
+      let li = document.createElement('LI');
+      li.id = `ttt${child}`;
+      li.className = "ttt-square";
+      // li.innerHTML = child;
+      ul.appendChild(li);
+    })
+    return ul;
+  }
   
-  bindEvents() {}
+  bindEvents() {
+      // let squares = document.querySelectorAll('.ttt-square');
+      // for(let i = 0; i < squares.length; i++){
+      //   squares[i].addEventListener('click', this.handleClick.bind(this))
+      // }
+      let squares = document.querySelector('.ttt-board');
+      squares.addEventListener('click', this.handleClick.bind(this))
+  }
 
-  handleClick(e) {}
+  handleClick(e) {
+    // debugger;
+    let element = e.target;
+    if(element.tagName!=='LI'){
+      return;
+    }
+    if(element.innerHTML === 'x' ||element.innerHTML === 'o' ){
+      alert('this move is invalid');
+      return;
+    }
+    let pos = Number(element.id.slice(3));
+    let row = Math.floor(pos/3);
+    let col = pos % 3;
+    element.innerHTML = this.game.currentPlayer;
 
-  makeMove(square) {}
+
+    if(element.innerHTML === 'x'){
+      element.classList.add('x');
+    }else{
+      element.classList.add('o');
+    }
+    this.game.playMove([row,col]);
+    this.displayWinner();
+  
+  }
+
+  makeMove(square) {
+
+  }
+
+  displayWinner() {
+    if(this.game.winner()){
+     alert(`congradulations to ${this.game.winner()}`);
+    }
+  }
 
 }
 
